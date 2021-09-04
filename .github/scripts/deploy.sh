037d1307fa182e5c8d1db73959cc1932c3c28ff1
Muhammad Imran <muhammad.imran@cern.ch>
2021-09-04 09:10:36 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -11,19 +11,20 @@ helm repo add --username=${HARBOR_USER} --password=${HARBOR_TOKEN} myrepo  https
 helm repo update
 helm repo list
 for chart in $(ls -d helm/*/Chart.yaml | xargs dirname); do
-          LOCAL_VERSION=$(grep -R "version:" ${chart}/Chart.yaml | awk '{print $2}')
-          if ! REMOTE_LATEST_VERSION="$(helm search repo myrepo/"${chart}" | grep myrepo/"${chart}" | awk '{print $2}')" ; then
-              echo "INFO There are no remote versions."
-              REMOTE_LATEST_VERSION=""
-          fi
-          if [ "${REMOTE_LATEST_VERSION}" = "" ] || \
-              [ "$(expr "${REMOTE_LATEST_VERSION}" \< "${LOCAL_VERSION}")" -eq 1 ]; then
-              helm dep update ${chart}
-              helm package ${chart}
-              #helm push --username=${HARBOR_USER} --password=${HARBOR_TOKEN} "${chart}-${LOCAL_VERSION}.tgz"  myrepo
-              set +x
-              curl --fail -F "chart=@${chart}-${LOCAL_VERSION}.tgz" -H "authorization: Basic $(echo -n ${HARBOR_USER}:${HARBOR_TOKEN} | base64)" https://registry.cern.ch/api/chartrepo/cmsweb/charts
-              set -x
-          fi
+echo $chart
+#          LOCAL_VERSION=$(grep -R "version:" ${chart}/Chart.yaml | awk '{print $2}')
+#          if ! REMOTE_LATEST_VERSION="$(helm search repo myrepo/"${chart}" | grep myrepo/"${chart}" | awk '{print $2}')" ; then
+#              echo "INFO There are no remote versions."
+#              REMOTE_LATEST_VERSION=""
+#          fi
+#          if [ "${REMOTE_LATEST_VERSION}" = "" ] || \
+#              [ "$(expr "${REMOTE_LATEST_VERSION}" \< "${LOCAL_VERSION}")" -eq 1 ]; then
+#              helm dep update ${chart}
+#              helm package ${chart}
+#              #helm push --username=${HARBOR_USER} --password=${HARBOR_TOKEN} "${chart}-${LOCAL_VERSION}.tgz"  myrepo
+#              set +x
+#              curl --fail -F "chart=@${chart}-${LOCAL_VERSION}.tgz" -H "authorization: Basic $(echo -n ${HARBOR_USER}:${HARBOR_TOKEN} | base64)" https://registry.cern.ch/api/chartrepo/cmsweb/charts
+#              set -x
+#          fi
 done
 
