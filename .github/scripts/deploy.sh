8e44de88f10a848d0ea6692bfebf281082102edb
Muhammad Imran <muhammad.imran@cern.ch>
2021-09-04 08:37:31 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo "Current directory" 
+pwd
 curl -o helm.tar.gz https://get.helm.sh/helm-v3.7.0-rc.2-linux-amd64.tar.gz; mkdir -p helm; tar zxvf helm.tar.gz -C helm; cp helm/linux-amd64/helm /usr/local/bin; rm -rf helm*
 
 helm repo add stable https://charts.helm.sh/stable
@@ -7,9 +8,7 @@ helm plugin install https://github.com/chartmuseum/helm-push
 helm repo add --username=${HARBOR_USER} --password=${HARBOR_TOKEN} myrepo  https://registry.cern.ch/chartrepo/cmsweb
 helm repo update
 helm repo list
-
-      pwd
-      cd helm
+cd helm
       for chart in $(ls -d */Chart.yaml | xargs dirname); do
           LOCAL_VERSION=$(grep -R "version:" ${chart}/Chart.yaml | awk '{print $2}')
           if ! REMOTE_LATEST_VERSION="$(helm search repo myrepo/"${chart}" | grep myrepo/"${chart}" | awk '{print $2}')" ; then
