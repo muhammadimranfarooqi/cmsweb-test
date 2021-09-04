49ff2aac10c170d01d4abb4573a31d07ec6e62e1
Muhammad Imran <muhammad.imran@cern.ch>
2021-09-04 09:14:09 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o helm.tar.gz https://get.helm.sh/helm-v3.7.0-rc.2-linux-amd64.tar.gz; mkdir -p helm; tar zxvf helm.tar.gz -C helm; cp helm/linux-amd64/helm /usr/local/bin; rm -rf helm*
+#curl -o helm.tar.gz https://get.helm.sh/helm-v3.7.0-rc.2-linux-amd64.tar.gz; mkdir -p helm; tar zxvf helm.tar.gz -C helm; cp helm/linux-amd64/helm /usr/local/bin; rm -rf helm*
 helm repo add stable https://charts.helm.sh/stable
 helm plugin install https://github.com/chartmuseum/helm-push
 helm repo add --username=${HARBOR_USER} --password=${HARBOR_TOKEN} myrepo  https://registry.cern.ch/chartrepo/cmsweb
